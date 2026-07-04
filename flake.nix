a9f1f9b59d467052a66c36e79f9653819f7aa21f
Choi Min Seok <cef1ys-gh@yahoo.com>
2026-07-04 04:12:38 +0000

    feat: initial blog setup with Zola + Nix Flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "My personal blog, built with Zola";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+          ];
+        };
+      }
+    );
+}
